fd9d71b31c3005c9334dcd256c83531a625b3b65
Geoffrey Rono <you@example.com>
2026-03-05 03:17:56 +0100

    M1: forward deep link intents in MainActivity and enable Flutter deeplinking

--- android/app/src/main/kotlin/com/flowai/app/MainActivity.kt
+++ android/app/src/main/kotlin/com/flowai/app/MainActivity.kt
@@ -1,5 +1,20 @@
 package com.flowai.app
 
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    Log.i("FlowAI", "onCreate intent data=" + intent?.dataString)
+  }
+
+  override fun onNewIntent(intent: Intent) {
+    super.onNewIntent(intent)
+    setIntent(intent)
+    Log.i("FlowAI", "onNewIntent data=" + intent.dataString)
+  }
+}
